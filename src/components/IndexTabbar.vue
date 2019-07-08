af0aa4c8a018308d039a8248df9dc28d6435799f
lius <18242078153@139.com>
2019-07-08 08:41:50 +0800

    woshigou

--- src/components/IndexTabbar.vue
+++ src/components/IndexTabbar.vue
@@ -1,20 +1,20 @@
 <template>
-    <div>
-        <ul>
-        <router-link to="/index/recommend" tag="li" activeClass="myactive">推荐</router-link>
-        <router-link to="/index/furniture" tag="li" activeClass="myactive">家具</router-link>
-        <router-link to="/index/jiaju" tag="li" activeClass="myactive">家居</router-link>
-        <router-link to="/index/active" tag="li" activeClass="myactive">活动</router-link>
-        </ul>
-    </div>
+  <div>
+    <ul>
+      <router-link to="/index/recommend" tag="li" activeClass="myactive">推荐</router-link>
+      <router-link to="/index/furniture" tag="li" activeClass="myactive">家具</router-link>
+      <router-link to="/index/jiaju" tag="li" activeClass="myactive">家居</router-link>
+      <router-link to="/index/active" tag="li" activeClass="myactive">活动</router-link>
+    </ul>
+  </div>
 </template>
 <style lang="scss" scoped>
-ul{
-display: flex;
-li{
+ul {
+  display: flex;
+  li {
     flex: 1;
     text-align: center;
-    font-size: 0.28rem
-}
+    font-size: 0.28rem;
+  }
 }
 </style>
