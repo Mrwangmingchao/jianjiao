24e8c7aa5ff6ab53fc341a4fd4f9ede7bcdeb5dd
lius <18242078153@139.com>
2019-07-09 16:46:15 +0800

    wodeyemian

--- src/views/productGroup.vue
+++ src/views/productGroup.vue
@@ -1,9 +1,15 @@
 <template>
-  <div>
-    productGroup
-    <ul>
-      <li v-for="item in datalist" :key='item.productId'>
-        <img v-lazy="item" :src="item.productImg"/>
+  <div id="box">
+    <ul
+      v-infinite-scroll="loadMore"
+      infinite-scroll-disabled="loading"
+      infinite-scroll-distance="10"
+    >
+      <li v-for="item in datalist" :key="item.productId">
+        <img :src="item.productImg" />
+        <p>{{item.productTitle}}</p>
+        <p>￥{{item.sellPrice}}</p>
+        <p>{{item.prizeOrSlogan}}</p>
       </li>
     </ul>
   </div>
@@ -18,19 +24,64 @@ Vue.use(Lazyload)
 export default {
   data () {
     return {
-      datalist: []
+      datalist: [],
+      num: 1
     }
   },
   mounted () {
-    axios
-      .get('/pages/productGroup/11902/products?currentPage=1&_=1562649396851')
-      .then(res => {
-        this.datalist = res.data.data
-        console.log(this.datalist)
-      })
+    //   console.log(this.$route.params.Groupid)
+    // axios
+    //   .get(`/pages/productGroup/${this.$route.params.Groupid}/products?currentPage=1&_=1562649396851`)
+    //   .then(res => {
+    //     this.datalist = res.data.data;
+    //     console.log(this.datalist);
+    //   });
   },
-  methods: {}
+  methods: {
+    loadMore () {
+      if (this.loading === false) return
+      this.loading = true
+      this.num++
+
+      axios
+        .get(
+          `/pages/productGroup/${this.$route.params.Groupid}/products?currentPage=${this.num}&_=1562649396851`
+        )
+        .then(res => {
+          console.log(res.data.data)
+          if (res.data.data.length === 0) {
+            this.loading = false
+            return
+          }
+          this.datalist = [...this.datalist, ...res.data.data]
+        })
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
+#box {
+  margin-bottom: 1rem;
+  > ul {
+    display: flex;
+    flex-wrap: wrap;
+    > li {
+      width: 50%;
+      background: white;
+      border: 1px solid rgb(245, 245, 245);
+      box-sizing: border-box;
+      img {
+        width: 100%;
+      }
+      > p {
+        width: 100%;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        padding: 0rem 0.2rem;
+        box-sizing: border-box;
+      }
+    }
+  }
+}
 </style>
