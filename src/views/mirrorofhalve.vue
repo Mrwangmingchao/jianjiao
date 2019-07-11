5830d615a1c89a85f6fbcbf094120409d4d020ad
lius <18242078153@139.com>
2019-07-11 15:06:41 +0800

    aaaaa

---
+++ src/views/mirrorofhalve.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <img src="https://img.wowdsgn.com/page/banners/55cd6c66-8c52-41f6-9976-23800ca16331_2dimension_750x752.jpg" alt="">
+        <img src="https://img.wowdsgn.com/page/banners/43364252-2272-40cc-8ed0-4ab91a2870d1_2dimension_750x264.jpg" alt="">
+        <img src="https://img.wowdsgn.com/page/banners/b0f2af0d-e7f7-4e57-84f8-84bf5e2389b2_2dimension_750x904.jpg" alt="">
+        <img src="https://img.wowdsgn.com/page/banners/59c81112-3079-481a-a5b8-6030014c8133_2dimension_750x847.jpg" alt="">
+        <img src="https://img.wowdsgn.com/page/banners/412f1cd1-3f05-4183-88f5-46e48f49a0f0_2dimension_750x960.jpg" alt="">
+        <img src="https://img.wowdsgn.com/page/banners/fe9af14f-8007-4dc0-8c1b-a42336112ef6_2dimension_750x969.jpg" alt="">
+        <img src="https://img.wowdsgn.com/page/banners/f9973c04-9d3f-4636-b0d3-7883cc18a3e1_2dimension_750x916.jpg" alt="">
+        <img src="https://img.wowdsgn.com/page/banners/b17cd177-eb49-4b20-94c4-623598f4d66f_2dimension_750x848.jpg" alt="">
+        <img src="https://img.wowdsgn.com/page/banners/0c85bc8e-68dd-48c1-8c6d-7df7aaa76db1_2dimension_750x871.jpg" alt="">
+        <img src="https://img.wowdsgn.com/page/banners/8afa7ae1-12a3-4d5e-ae31-ad7f941fb773_2dimension_750x259.jpg" alt="">
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="scss" scoped>
+    div{
+        img{
+            width:100%;
+        }
+    }
+</style>
